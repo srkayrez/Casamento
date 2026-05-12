bd54ad9756e05733ef47bf02df1368fa98228d2f
srkayrez <srkayrez@gmail.com>
2026-05-12 14:21:24 -0300

    removed contdownc omponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CountdownComponent } from "./timer/timer";
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { Menu } from './menu/menu';
@@ -8,12 +7,11 @@ import { Menu } from './menu/menu';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Garanta que isso esteja aqui se for Angular 18/19
-  imports: [CountdownComponent, Menu, RouterOutlet], // Apenas uma vez
+  standalone: true, 
+  imports: [Menu, RouterOutlet], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = signal('casamento');
-  // Coloque uma data de 2025 ou 2026 para testar o contador rodando
 }
